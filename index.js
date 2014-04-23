35149de93a49a7cc14178fccbb781698062459f0
DBond <dbond@bond.local>
2014-04-23 11:08:56 -0500

    rewrite

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 'use strict';
 var through = require('through2'),
     fs = require('fs'),
-    prefixr = require("html-prefixer");
+    url = require("url"),
+    trumpet = require("trumpet"),
+    concat  = require("concat-stream"),
+    _prefixer;
+
+_prefixer = function(options, attr) {
+    return function(node) {
+        node.getAttribute(attr, function(uri) {
+            var output;
+
+            uri = url.parse(uri, false, true);
+
+            // No sense in trying to work with these monsters
+            if(uri.host || !uri.path) {
+                return;
+            }
+
+            node.setAttribute(attr, url.resolve(options.prefix, uri.path));
+        });
+    };
+};
 
 module.exports = function(options) {
-
+  
   return through.obj(function(file, enc, cb) {
+    var tr = trumpet();
+
+    tr.selectAll("script[src]", _prefixer(options, "src"));
+    tr.selectAll("link[href]",  _prefixer(options, "href"));
+    tr.selectAll("img[src]",    _prefixer(options, "src"));
 
     var stream = fs.createReadStream(file.path);
     
-    prefixr(stream, options, function(err, text) {
-      if(err) {
-          console.log(err)
-      }
-
-      file.contents = text;
+    // No prefix
+    if(!options.prefix)
+        cb(null, file);
+    
+    tr.pipe(concat(function concatDone(data) {
+      file.contents = data;
       stream.close();
-      cb(err, file);
-    });
+      cb(null, file);
+    }));
 
+    stream.pipe(tr);    
   });
-}; 
+};
